7c0c29bfab09a5231c34a76e28e2e74f0ee5fe37
Bendik Egenes Dyrli <skandix@users.noreply.github.com>
2015-10-06 11:26:08 +0200

    Create instakill.bat
    script to easily kill all the processes i was using, to quicker leave work.. was set to the hotkey "ctrl + shift + alt + insert"

---
+++ instakill.bat
@@ -0,0 +1,12 @@
+@echo off
+Echo killing all Programs that have been choosen!
+taskkill /F /IM putty.exe
+taskkill /F /IM chrome.exe
+taskkill /F /IM skype.exe
+taskkill /F /IM cmd.exe
+taskkill /F /IM iexplore.exe
+taskkill /F /IM pageant.exe
+taskkill /F /IM Greenshot.exe
+taskkill /F /IM explorer.exe
+start explorer.exe
+pause
