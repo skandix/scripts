7a83ece67d5e36e21303b504fb2cdbf96e547d16
Bendik Egenes Dyrli <skandix@users.noreply.github.com>
2016-01-10 20:33:04 +0100

    Update optimus_prime.py

--- optimus_prime.py
+++ optimus_prime.py
@@ -1,11 +1,9 @@
-#Iterates triough a list, spits all the number out
-#Then checks if the number is a prime number.
-
 def optimus_prime(prime,iterations=101):
 	print ""
 	print "\nChoosen Prime %s \n" % prime
 	for i in xrange(iterations):
-		print is_prime(prime**i % 29)
+		print (prime**i % 29)
+		#print is_prime(prime**i % 29)
 
 def is_prime(intake):
 	print "%s a prime number?" % intake,
