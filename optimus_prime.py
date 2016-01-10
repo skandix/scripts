066edb293f0c0feba867521f175c6488c98aba2c
Bendik Egenes Dyrli <skandix@users.noreply.github.com>
2016-01-10 20:34:51 +0100

    Update optimus_prime.py

--- optimus_prime.py
+++ optimus_prime.py
@@ -1,13 +1,12 @@
-def optimus_prime(prime,iterations=101):
+def optimus_prime(prime,iterations=8):
 	print ""
 	print "\nChoosen Prime %s \n" % prime
 	for i in xrange(iterations):
-		print (prime**i % 29)
-		#print is_prime(prime**i % 29)
+		print (prime**i % 29),
+		#print is_prime(prime**i % 29),
 
 def is_prime(intake):
-	print "%s a prime number?" % intake,
-	print all(intake % p for p in xrange(2, intake)),
+	return all(intake % p for p in xrange(2, intake))
 
 optimus_prime(3301)
 optimus_prime(1033)
