a6e7a8376fb0b483d7aa952299e81aac0ac0adc1
BuildTools <bendik.dyrli@gmail.com>
2016-06-03 12:20:28 +0200

    made img downloader for 4chanthreads with regexpression. suuper fast...

---
+++ py/4chanGetImg.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import urllib2
+import re
+import wget
+import time
+#import os
+
+def motd():
+
+	print " _  _     ___ _                 "
+	print "| || |   / __\ |__   __ _ _ __  "
+	print "| || |_ / /  | '_ \ / _` | '_ \ "
+	print "|__   _/ /___| | | | (_| | | | |"
+	print "   |_| \____/|_| |_|\__,_|_| |_|"
+	print "                                "
+	print " REGEX GETimg DOWNLOADER \n"
+
+def getImgz(url):
+	black_magic = re.compile(ur'<img[^s]\w+\S+[.png|.jpg|.jpeg]')
+	lol = urllib2.urlopen(url)
+	fagz = lol.read()
+
+	loot = re.findall(black_magic, fagz)
+
+	for i in loot:
+		#just replace shit, with nothing, if you don't replace the s, you'll only get thumbnails
+		clean = str(i.replace('<img src="//', '').replace("s", ""))
+		print wget.download("http://"+clean)
+	print len(clean)
+
+motd()
+time.sleep(2)
+getImgz("")
