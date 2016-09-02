765817c1ad1c99190972191ab136e661448f8d8a
Bendik <skandix@users.noreply.github.com>
2016-09-02 14:17:38 +0200

    Update graph.py

--- py/graph.py
+++ py/graph.py
@@ -4,6 +4,9 @@ from pygal.style import DarkSolarizedStyle
 import math
 import random
 
+#getting started with pygal
+# http://www.blog.pythonlibrary.org/2015/04/16/using-pygal-graphs-in-flask/
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -33,8 +36,6 @@ def getGraphed():
 	r = random.randint(1,1000)
 	tan_chart.x_labels = "tan of random {:d}".format(r)
 	tan_chart.add("tan of r", math.tan(r))
-
-
 
 	html="""<html><head><title>{:s}</title></head><body><br></br>{:s}<br></br>{:s}<br></br>{:s}<br></br>{:s}<br></br></body></html>""".format(title, bar_chart.render(), cos_chart.render(), sin_chart.render(), tan_chart.render())
 	return html
