aff25e6f3f5bf646aa0420927748413ccb5c9eec
Bendik <skandix@users.noreply.github.com>
2016-10-23 01:48:23 +0200

    Create getTemp.py

---
+++ py/getTemp.py
@@ -0,0 +1,16 @@
+# reads out temp from serial
+import serial
+
+buffer = []
+
+def getTemp():
+	ser = serial.Serial('COM4', 9600, timeout=0)
+	while 1:
+	    if ser.inWaiting() == long(5) or ser.inWaiting() == long(6) or ser.inWaiting() == long(4):
+	    	#print "\nbytes", ser.inWaiting()
+	        val = ser.readline(ser.inWaiting())
+	    	buffer.append(str(val))
+		return buffer[-1]
+
+for j in range(50):
+	print getTemp()
