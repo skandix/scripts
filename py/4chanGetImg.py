8c3e3ccb6232f30f42bb16e3443a71aeb91a3104
skandix <skandix@skandixGit>
2017-01-07 04:29:43 +0100

    fixed 4chan script, to check for dupes, see that the string does not contain unknown, makes a folder with the thread id as name

--- py/4chanGetImg.py
+++ py/4chanGetImg.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-import urllib2
+import requests
 import re
 import wget
 import time
 import argparse
+import os
+
+resp = requests.get
+foundImg = []
+debugging = True
 
 def motd():
 
@@ -16,18 +21,51 @@ def motd():
 	print " REGEX GETimg DOWNLOADER \n"
 
 def getImgz(url):
-	motd()
-	black_magic = re.compile(ur'<img[^s]\w+\S+[.png|.jpg|.jpeg]')
-	lol = urllib2.urlopen(url)
-	fagz = lol.read()
+        urlID = url[33:]
 
-	loot = re.findall(black_magic, fagz)
+	black_magic = re.compile(ur'<img[^s]\w+\S+[.png|.jpg|.jpeg]')
+        lol = resp(url).text
+
+	loot = re.findall(black_magic, lol)
+        
+        if not os.path.exists(urlID) or os.path.exists("../"+urlID):
+            try:
+                os.mkdir(urlID)
+            except OSError as oserr_mkdir:
+                if debugging:
+                    print ("OS Error: {0}".format(oserr_mkdir))
+            else:   
+                os.chdir(urlID)
+
+
+        elif os.path.exists(urlID):
+            os.chdir(urlID)
+            print "dir changes"
+           
+#        try:
+        for j in os.listdir("../"+urlID):
+            foundImg.append(j)
+#        except OSError as oserr:
+#            if debugging:
+#                print ("OSError: {0}".format(oserr))
 
 	for i in loot:
-		#just replace shit, with nothing, if you don't replace the s, you'll only get thumbnails
-		clean = str(i.replace('<img src="//', '').replace("s", ""))
-		print wget.download("http://"+clean)
-	print len(clean)
+        	clean = str(i.replace('<img src="//', '').replace("s", ""))
+
+		if clean.count("unknown"):                
+                    print ("bad img, not downloading")
+
+                else:
+                    try:
+                        foundImg.index(clean[13:])
+                    except ValueError as valueerr:
+                        if debugging:
+                            print("Value Error: {0}".format(valueerr))   
+                        print (wget.download("http://"+clean))
+                    else:
+                        print ("Skipping")
+        if debugging:            
+        	print len(clean)
 
 
 parser = argparse.ArgumentParser()
@@ -35,5 +73,10 @@ parser.add_argument("--url")
 args = parser.parse_args()
 
 
-# Takes inn url as --url <insert url>
-getImgz(args.url)
+# Takes inn url as --url <insert u
+motd()
+while resp(args.url).status_code != 404:
+    getImgz(args.url)
+
+print ("Thread is Dead, RIP {0}".format(args.url))
+
