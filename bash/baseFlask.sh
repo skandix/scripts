4f13c681ac3bf446c65fe1206880a280e49db4f0
skandix <skandix@users.noreply.github.com>
2017-03-15 11:08:37 +0000

    Create baseFlask.sh

---
+++ bash/baseFlask.sh
@@ -0,0 +1,12 @@
+# i'm lazy, made this so i can quickly get started and shit... mkay
+mkdir templates
+curl https://gist.githubusercontent.com/skandix/823eaa3dd96a9a8c2d3e3a8e279ef2e8/raw/c5aaf1f43a44e7ff5bb304184e82045b7855e1ba/index.html > templates/index.html
+mkdir static
+mkdir static/css
+mkdir static/js
+mkdir static/img
+mkdir modules
+touch modules/__init__.py
+curl https://raw.githubusercontent.com/skandix/scripts/master/404.html > templates/404.html
+curl https://gist.githubusercontent.com/skandix/823eaa3dd96a9a8c2d3e3a8e279ef2e8/raw/c5aaf1f43a44e7ff5bb304184e82045b7855e1ba/flask_boilerplate.py > main.py
+curl https://gist.githubusercontent.com/skandix/823eaa3dd96a9a8c2d3e3a8e279ef2e8/raw/89bab1e10b1cdae30d624db4529372f95139896d/requirements.txt > requirements.txt
