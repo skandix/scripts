11710363f922ace99f5c97f3d6a5222eeaf42823
skandix <skandix@datapor.no>
2017-06-02 11:54:37 +0200

    Update choco_prog.bat

--- batch/choco_prog.bat
+++ batch/choco_prog.bat
@@ -7,12 +7,12 @@ choco install filezilla -y
 choco install git -y
 choco install teamspeak -y
 choco install steam -y
-choco install utorrent -y --allow-empty-checksums
+choco install utorrent -y --ignore-checksums
 choco install battle.net -y --allow-empty-checksums
 choco install 7zip -y
 choco install javaruntime -y
 choco install procmon -y
-choco install teamviewer -y --allow-empty-checksums
+choco install teamviewer -y --ignore-checksums
 choco install wireshark -y
 choco install spotify -y
 choco install discord -y
@@ -21,11 +21,11 @@ choco install jre8 -y
 choco install foobar2000 -y
 choco install veracrypt -y
 choco install vim -y
-choco install github -y
+choco install github -y --ignore-checksums
 choco install openvpn -y
 choco install firefox -y
-choco install cygwin -y --allow-empty-checksums
-choco install tor-browser -y --allow-empty-checksums
+choco install cygwin -y --ignore-checksums
+choco install tor-browser -y --ignore-checksums
 choco install windows-tweaker -y
 choco install mpc-hc -y --allow-empty-checksums
 choco install python2 -y
