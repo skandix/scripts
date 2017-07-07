bdff4cdc649d043a74eaf1ef21cbbc163173eede
skandix <skandix@datapor.no>
2017-07-07 15:07:11 +0200

    Update datamine_delete.bat

--- batch/datamine_delete.bat
+++ batch/datamine_delete.bat
@@ -1,26 +1,19 @@
-ECHO OFF
+:: used at old workplace for truly removing everything from the monitoring software we used.
+@ECHO OFF
 TITLE Automated Cleaning/Remover of Dataminer
-ECHO.  
 ECHO Starting automated EXTERMATION of Dataminer
-ECHO. 
 PING -n 3 127.0.0.1>nul
 CLS
-ECHO. 
+
 ECHO Cleaning Skyline OneClick 
-ECHO. 
-PING -n 3 127.0.0.1>nul 
 RD /S /Q "C:\%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Skyline Communications"
-PING -n 3 127.0.0.1>nul
 RD /S /Q "C:\ProgramData\Skyline"
-PING -n 3 127.0.0.1>nul
 CLS
-ECHO.
+
 ECHO Deleting Dataminer Cube Standalone version x.x.x.x
-ECHO.
 wmic product where name="DataMiner Cube *.*.*.*" call uninstall /nointeractive > log.txt
-CLS
-ECHO.  
-ECHO Done Running Automated Dataminer Remover
-ECHO. 
+CLS 
+
+ECHO Done Running Automated Dataminer Remover 
 PING -n 3 127.0.0.1>nul 
 EXIT
