6cf18a4ed3a5e54650f43342626267c49660482f
Bendik λ <skandix@datapor.no>
2017-08-20 15:59:27 +0200

    Update 4chanGetImg.py
    don't have time to fix this..

--- py/4chanGetImg.py
+++ py/4chanGetImg.py
@@ -2,18 +2,15 @@
 from sys import platform as _os
 import requests
 import re
+import wget
 import time
-import shutil
 import argparse
 import os
 
-#resp = requests.get
+resp = requests.get
 foundImg = []
 debugging = True
 workdir = os.getcwd()
-
-def resp(url, strem=False):
-    return requests.get(url, stream=strem)
 
 def clearTerm():
     if _os == "linux" or _os == "linux2" or _os == "cygwin" or _os =="darwin":
@@ -84,9 +81,9 @@ def getImgz(url):
                         if debugging:
                             print("Value Error: {0}".format(valueerr))   
                      
-                        with open(img_name, 'wb+') as file:                        
-                            shutil.copyfileobj(resp("http://"+clean.raw, True).raw, file)
-                        return "Downloaded:\t{:s}".format(img_name)
+                        print "input wget", clean
+                        print (wget.download("http://"+clean))
+                        print "\n"
                     
                     else:                        
                         waiting("Checking thread for new Images")
