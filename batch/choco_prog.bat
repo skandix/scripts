abe21654b566e22c89e7ce14bbf261f54a36e008
Bendik λ <skandix@datapor.no>
2019-03-19 00:21:12 +0100

    Update choco_prog.bat

--- batch/choco_prog.bat
+++ batch/choco_prog.bat
@@ -1,34 +1,34 @@
 @echo off 
 md C:\urxvt\keys
 md C:\urxvt\misc
+choco install firefox -y
 choco install 7zip -y
-choco install openhardwaremonitor -y
+choco install twitch -y
+choco install vscode -y
 choco install audacity -y
 choco install battle.net -y --allow-empty-checksums
-choco install burp-suite-free-edition -y
 choco install discord -y
 choco install filezilla -y
 choco install foobar2000 -y
-choco install github -y --ignore-checksums
+choco install github-desktop -y
+choco install plexmediaplayer -y
 choco install jre8 -y
+choco install mpv -y
 choco install keepass -y 
-choco install megasync -y
 choco install firefox -y
-choco install mumble -y
+choco install ontopreplica -y
 choco install obs-studio -y
-choco install postman -y
-choco install python2 -y
+choco install python -y
 choco install sharex -y
 choco install slack -y
 choco install speccy -y
-choco install speedfan -y
 choco install spotify -y
-choco install sublimetext3 -y
 choco install teamspeak -y
 choco install teamviewer -y --ignore-checksums
 choco install telegram -y
 choco install veracrypt -y
 choco install windows-tweaker -y
+choco install vibrancegui -y
 choco install wireshark -y
 choco install steam -y
 choco install tor-browser -y --ignore-checksums
@@ -36,6 +36,7 @@ choco install puttytray -y
 choco install wget -y
 wget https://the.earth.li/~sgtatham/putty/latest/w64/pageant.exe
 wget https://loot.datapor.no/dzikvy9v.zip
+wget https://app-updates.plays.tv/builds/PlaysSetup.exe
 move https://the.earth.li/~sgtatham/putty/latest/w64/pageant.exe C:/urxvt/keys
 cd C:/urxvt/keys/
 pageant.exe skandix.ppk
