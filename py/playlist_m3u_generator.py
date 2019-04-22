6b101119159ed95d238e03f6134dfaefe1bcb590
Bendik λ <skandix@datapor.no>
2019-04-22 21:09:54 +0200

    Update playlist_m3u_generator.py

--- py/playlist_m3u_generator.py
+++ py/playlist_m3u_generator.py
@@ -1,10 +1,20 @@
-import os
+import os, argparse
 
-for root, dirs, files in  os.walk('/mnt/Media/Music'):
+audioExt = ['.mp3', '.wav', '.flac']
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dir", "-dir", help="what music dir do you want to generate a playlist file of")
+
+args = parser.parse_args()
+
+for root, dirs, files in  os.walk(args.dir):
     for name in files:
-        if ".mp3" in name or ".wav" in name:
-            with open('playlist.m3u','a+') as file:
-                file.write(os.path.join(root,name))
-                file.write("\n")
-        else:
-            pass
+        for ext in audioExt:
+            if ext in name:
+                with open(args.dir+'playlist.m3u','a+') as file:
+                    file.write(os.path.join(root,name))
+                    file.write("\n")
+            else:
+                pass
+
+    print ("Done")
