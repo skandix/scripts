d6c7398f16410806494b8a84f28c62fa765d1295
Bendik λ <skandix@datapor.no>
2019-11-03 17:28:40 +0100

    Update requestsDL.py

--- py/requestsDL.py
+++ py/requestsDL.py
@@ -2,7 +2,6 @@ import requests
 import shutil
 
 def download(url, name):
-    url = "https://"+url
     stream = (requests.get(url, stream=True))
     with open(name, 'wb+') as file:
         shutil.copyfileobj(stream.raw, file
