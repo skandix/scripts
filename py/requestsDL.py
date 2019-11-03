e01efd99c895fbfda83a7924c138a02279b80b19
Bendik λ <skandix@datapor.no>
2019-11-03 17:27:48 +0100

    Create requestsDL.py

---
+++ py/requestsDL.py
@@ -0,0 +1,8 @@
+import requests
+import shutil
+
+def download(url, name):
+    url = "https://"+url
+    stream = (requests.get(url, stream=True))
+    with open(name, 'wb+') as file:
+        shutil.copyfileobj(stream.raw, file
