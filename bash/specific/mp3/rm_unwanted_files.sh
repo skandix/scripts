140d55cbe19b494d060dfe963361bd8099c7d67b
Bendik λ <bendik.dyrli@uia.no>
2019-12-08 21:10:30 +0100

    Update rm_unwanted_files.sh

--- bash/specific/mp3/rm_unwanted_files.sh
+++ bash/specific/mp3/rm_unwanted_files.sh
@@ -2,14 +2,15 @@
 find . -iname '*.mp3' -exec sh -c 'curfname="{}";\
 length=`mp3info -p "%m" "$curfname" 2>&1`; \
 errors=`mp3info -p "%b" "$curfname" 2>&1`; \
-notmp3=`echo $errors | grep corrupt`; \
+notmp3=`echo $errors | grep corrupt`;
+songLength=10 # Specify a value, if a songs length is greater than the value it will be deleted.
 if [ "$notmp3" ]; \
   then rm -rfv "$curfname"; \
 else \
   if [ $errors -gt 0 ];\
      then rm -rfv "$curfname"; \
   fi; \
-  if [ $length -gt 10 ];\
+  if [ $length -gt $songLength ];\
      then rm -rfv "$curfname"; \
   fi; \
 fi' \;
